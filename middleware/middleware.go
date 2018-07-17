70c480500902d7062b4c233d754710697e21cb6c
ngocvantu <tunguyen4078@gmail.com>
2018-07-17 21:52:09 +0700

    add check http https

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -9,12 +9,24 @@ import (
 var store = db.Store
 
 func CheckLogin(f http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter	, r *http.Request) {
 
 		session, _ :=  store.Get(r, "user-session")
 
 		if strings.HasPrefix(r.RequestURI, "/login") {
 			if session.Values["email"] != nil {
+				email := session.Values["email"]
+
+				// delete to re-create session
+				session.Options.MaxAge = -1
+				session.Save(r, w)
+
+				session, _ :=  store.Get(r, "user-session")
+				session.Values["email"] = email
+				session.Options.HttpOnly = true
+				session.Options.MaxAge = 300
+				session.Save(r, w)
+
 				http.Redirect(w,r,"/topic",http.StatusFound)
 				return
 			}
@@ -22,6 +34,18 @@ func CheckLogin(f http.HandlerFunc) http.HandlerFunc {
 			if session.Values["email"] == nil {
 				http.Redirect(w,r,"/login",http.StatusFound)
 				return
+			} else {
+				email := session.Values["email"]
+
+				// delete to re-create session
+				session.Options.MaxAge = -1
+				session.Save(r, w)
+
+				session, _ :=  store.Get(r, "user-session")
+				session.Values["email"] = email
+				session.Options.HttpOnly = true
+				session.Options.MaxAge = 300
+				session.Save(r, w)
 			}
 		}
 
