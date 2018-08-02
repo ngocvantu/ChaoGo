e58dfbaeabc86f7968137c679cf19ff7442dfe49
ngocvantu <tunguyen4078@gmail.com>
2018-08-02 15:17:30 +0700

    add redirect http to https

--- main/main.go
+++ main/main.go
@@ -36,7 +36,7 @@ func IndexController(w http.ResponseWriter, r *http.Request){
 
 func redirectToHttps(w http.ResponseWriter, r *http.Request) {
 	// Redirect the incoming HTTP request. Note that "127.0.0.1:8081" will only work if you are accessing the server from your local machine.
-	http.Redirect(w, r, "https://tunguyen.top:8081/"+r.RequestURI, http.StatusMovedPermanently)
+	http.Redirect(w, r, "https://tunguyen.top:8081"+r.RequestURI, http.StatusMovedPermanently)
 }
 
 func main() {
@@ -57,7 +57,7 @@ func main() {
 
 	go http.ListenAndServeTLS(":8081", "server.crt", "server.key",  context.ClearHandler(http.DefaultServeMux))
 
-	err := http.ListenAndServe(":8081", http.HandlerFunc(redirectToHttps))
+	err := http.ListenAndServe(":80", http.HandlerFunc(redirectToHttps))
 	fmt.Print(err)
 }
 func checkErr(e error) {
