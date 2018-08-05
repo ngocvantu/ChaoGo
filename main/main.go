9029fc71a4c4d20d15d5ba187a2bf835c610f759
ngocvantu <tunguyen4078@gmail.com>
2018-08-05 22:29:06 +0700

    test port 80

--- main/main.go
+++ main/main.go
@@ -34,6 +34,11 @@ func IndexController(w http.ResponseWriter, r *http.Request){
 	t.Execute(w,commonParam)
 }
 
+func redirectToHttps(w http.ResponseWriter, r *http.Request) {
+	// Redirect the incoming HTTP request. Note that "127.0.0.1:8081" will only work if you are accessing the server from your local machine.
+	http.Redirect(w, r, "https://tunguyen.top"+r.RequestURI, http.StatusMovedPermanently)
+}
+
 func main() {
 	http.HandleFunc("/", IndexController)
 
@@ -50,7 +55,7 @@ func main() {
 
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 
-	//e := http.ListenAndServe(":80", context.ClearHandler(http.DefaultServeMux))
+	go http.ListenAndServe(":80", context.ClearHandler(http.HandlerFunc(redirectToHttps)))
 
 	e := http.ListenAndServeTLS(":443", "server.crt", "server.key",  context.ClearHandler(http.DefaultServeMux))
 	if e != nil {
