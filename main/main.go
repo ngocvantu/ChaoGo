9e54acec11c993c26687abfcb22baf66caeb6026
ngocvantu <tunguyen4078@gmail.com>
2019-01-01 22:53:39 +0700

    Update main.go

--- main/main.go
+++ main/main.go
@@ -39,22 +39,22 @@ func redirectToHttps(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", IndexController)
+// 	http.HandleFunc("/", IndexController)
 
-	http.HandleFunc("/login", middleware.CheckLogin(user.LoginController))
-	http.HandleFunc("/logout", middleware.CheckLogin(user.LogoutController))
+// 	http.HandleFunc("/login", middleware.CheckLogin(user.LoginController))
+// 	http.HandleFunc("/logout", middleware.CheckLogin(user.LogoutController))
 
-	http.HandleFunc("/topic", middleware.CheckLogin(topic.TopicController))
-	http.HandleFunc("/delete", middleware.CheckLogin(topic.DeleteTopic))
-	http.HandleFunc("/edittopic", middleware.CheckLogin(topic.EditTopic))
+// 	http.HandleFunc("/topic", middleware.CheckLogin(topic.TopicController))
+// 	http.HandleFunc("/delete", middleware.CheckLogin(topic.DeleteTopic))
+// 	http.HandleFunc("/edittopic", middleware.CheckLogin(topic.EditTopic))
 
-	http.HandleFunc("/knowledge", middleware.CheckLogin(knowledge.KnowledgeController))
-	http.HandleFunc("/editknowledge", middleware.CheckLogin(knowledge.EditKnowledge))
-	http.HandleFunc("/deleteknowledge", middleware.CheckLogin(knowledge.Deleteknowledge))
+// 	http.HandleFunc("/knowledge", middleware.CheckLogin(knowledge.KnowledgeController))
+// 	http.HandleFunc("/editknowledge", middleware.CheckLogin(knowledge.EditKnowledge))
+// 	http.HandleFunc("/deleteknowledge", middleware.CheckLogin(knowledge.Deleteknowledge))
 
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+// 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
  
-	go http.ListenAndServe(":80", context.ClearHandler(http.HandlerFunc(redirectToHttps)))
+// 	go http.ListenAndServe(":80", context.ClearHandler(http.HandlerFunc(redirectToHttps)))
 
 	http.ListenAndServeTLS(":443", "server.crt", "server.key",  context.ClearHandler(http.DefaultServeMux))
 
